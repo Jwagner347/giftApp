4ffb2b74d1193e9711fdf863f9c120c09219a071
Jeff Wagner <jwagner@thoughtworks.com>
2017-03-30 08:26:57 -0400

    add .eslintrc remove jasmine examples

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "plugins": [
+      "import"
+  ],
+  "env": {
+    "node": true
+  },
+  "rules": {
+    "comma-dangle": ["error", "never"],
+    "arrow-parens": ["error", "always"],
+    "arrow-body-style": ["error", "always"]
+  }
+};
