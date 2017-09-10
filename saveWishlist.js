c1daa1b3d1d094180d7a5864e43116c107ff8ec5
Jeff Wagner <jwagner@thoughtworks.com>
2017-09-10 16:37:48 -0500

    save documents to mongoDb instead of a json filej

--- saveWishlist.js
+++ saveWishlist.js
@@ -1,47 +1,67 @@
 const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017/myproject';
+
 
 module.exports = (wishlistItems, name) => {
-  const wishlistToSave = { wishlistItems, name };
-  let dataStoreFile;
-  let parsedDataStoreFile;
-  let dataStoreJSON;
 
-  if (!fs.existsSync('./wishlistDataStore.json')) {
-    fs.writeFileSync('./wishlistDataStore.json', JSON.stringify({
-      wishlists: {
-        1: {
-          id: 1,
-          wishlist: wishlistToSave
-        }
+  const newWishlist = ({ document, db, nameOfCollection, callback }) => {
+    const collection = db.collection(nameOfCollection);
+    collection.insertOne(document, (err, result) => {
+      if (err) {
+        console.log(err);
       }
-    }));
-  } else {
-    dataStoreFile = fs.readFileSync('./wishlistDataStore.json');
-    parsedDataStoreFile = JSON.parse(dataStoreFile);
-    const wishlistsIds = Object.keys(parsedDataStoreFile.wishlists);
-    const wishlistAlreadyExists = (wishlists, nameToCheck) => {
-      let existingWishlistId;
-      wishlistsIds.forEach((wishlistId) => {
-        if (wishlists[`${wishlistId}`].wishlist.name === nameToCheck) {
-          existingWishlistId = wishlistId;
+      callback(result);
+    });
+  };
+
+  const wishlistToSave = { wishlistItems, name };
+
+  MongoClient.connect(url, (err, db) => {
+    if (err) {
+      console.log('error!! ', err);
+    } else {
+      newWishlist({ document: wishlistToSave,
+        db,
+        nameOfCollection: 'wishlists',
+        callback: (result) => {
+          console.log(result);
         }
       });
-      return existingWishlistId;
-    };
-    const existingWishlist = wishlistAlreadyExists(parsedDataStoreFile.wishlists, name);
-    if (existingWishlist) {
-      parsedDataStoreFile.wishlists[`${existingWishlist}`] = {
-        id: existingWishlist,
-        wishlist: wishlistToSave
-      };
-    } else {
-      const newWishlistNumber = wishlistsIds.length + 1;
-      parsedDataStoreFile.wishlists[`${newWishlistNumber}`] = {
-        id: newWishlistNumber,
-        wishlist: wishlistToSave
-      };
+      db.close();
     }
-    dataStoreJSON = JSON.stringify(parsedDataStoreFile);
-    fs.writeFileSync('./wishlistDataStore.json', dataStoreJSON);
-  }
+  });
+
+
+    // collection.findOneAndUpdate({ name }, { $set: wishlistToSave }, {
+    //   returnOriginal: false
+    // }, (error, result) => {
+    //   console.log('result: ', result);
+    // });
+
+  // const wishlistAlreadyExists = (wishlists, nameToCheck) => {
+  //   let existingWishlistId;
+  //   wishlistsIds.forEach((wishlistId) => {
+  //     if (wishlists[`${wishlistId}`].wishlist.name === nameToCheck) {
+  //       existingWishlistId = wishlistId;
+  //     }
+  //   });
+  //   return existingWishlistId;
+  // };
+  // const existingWishlist = wishlistAlreadyExists(parsedDataStoreFile.wishlists, name);
+  // if (existingWishlist) {
+  //   parsedDataStoreFile.wishlists[`${existingWishlist}`] = {
+  //     id: existingWishlist,
+  //     wishlist: wishlistToSave
+  //   };
+  // } else {
+  //   const newWishlistNumber = wishlistsIds.length + 1;
+  //   parsedDataStoreFile.wishlists[`${newWishlistNumber}`] = {
+  //     id: newWishlistNumber,
+  //     wishlist: wishlistToSave
+  //   };
+  // }
+  // dataStoreJSON = JSON.stringify(parsedDataStoreFile);
+  // fs.writeFileSync('./wishlistDataStore.json', dataStoreJSON);
 };
