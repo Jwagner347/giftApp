d91b30692a2f96f3aec0851275bc495673cf1503
Jeff Wagner <jwagner@thoughtworks.com>
2017-12-13 13:33:46 -0600

    add validations for wishlistItems and name in update wishlist

--- updateWishlistFor.js
+++ updateWishlistFor.js
@@ -3,6 +3,14 @@ const MongoClient = require('mongodb').MongoClient;
 const dbUrl = process.env.DB_URL;
 
 module.exports = (name, wishlistItems) => {
+  if (!Array.isArray(wishlistItems) || !wishlistItems.length) {
+    throw new Error('Wishlist items must be an array with at least one item');
+  }
+
+  if (typeof name !== 'string' || !name) {
+    throw new Error('Name must be a string of at least one character in length');
+  }
+
   return MongoClient.connect(dbUrl)
     .then((db) => {
       const collection = db.collection('wishlists');
