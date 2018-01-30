464a8e0eb6124a638a653632bb8645c917814d1d
Jeff Wagner <jwagner@thoughtworks.com>
2018-01-29 22:54:43 -0600

    allow cors from localhost

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const userController = require('./controllers/user');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/wishlist/:userId', (req, res) => {
   wishlistController.get(req, res);
